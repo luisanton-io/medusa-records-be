8a2c3b390614bd86b5daef88e9877e56d0a62a44
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-09-17 13:11:28 +0200

    Update userSchema.ts

--- src/services/login/userSchema.ts
+++ src/services/login/userSchema.ts
@@ -1,14 +1,19 @@
 import mongoose, { Schema } from "mongoose"
 import bcrypt from "bcryptjs"
 import validator from "validator"
-import { getModelForClass, prop } from "@typegoose/typegoose"
+import { arrayProp, getModelForClass, pre, prop } from "@typegoose/typegoose"
+import { Credentials } from "../../models/Credentials"
 
-class JwtToken {
-  @prop()
-  token!: string
-}
+@pre<User>('save', async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8)
+  }
+  next()
+})
 
-class User {
+export class User {
+  public _id!: Schema.Types.ObjectId;
+
   @prop({
     required: true,
     validate: async (value) => {
@@ -23,94 +28,19 @@ class User {
   @prop({ required: true, minlength: 2 })
   public password!: string
 
-  @prop()
-  public refreshTokens?: JwtToken[]
+  @prop({ type: () => [String], default: [] })
+  public refreshTokens!: string[]
+
+  public static async findByCredentials({ email, password }: Credentials) {
+    const user = await UserModel.findOne({ email })
+    if (!user) return null
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    return isMatch ? user : null
+  }
 }
+
 
 const UserModel = getModelForClass(User)
 
 export default UserModel
-
-// const UserSchema: Schema = new Schema(
-//   {
-//     email: {
-//       type: String,
-//       required: true,
-//       lowercase: true,
-//       validate: async (value) => {
-//         if (v.isEmail(value)) {
-//           let user = await UserModel.findOne({ username: value })
-//           if (user)
-//             throw new Error("This username already exists!")
-//         }
-//         else
-//           throw new Error("Invalid email.")
-//       }
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//       // minlength: 7,
-//     },
-//     refreshTokens: [
-//       {
-//         token: {
-//           type: String,
-//           required: true
-//         }
-//       }
-//     ]
-//   }
-// )
-
-// UserSchema.statics.findByCredentials = async (email, password) => {
-//   const user = await UserModel.findOne({ email })
-
-//   if (user) {
-//     const isMatch = await bcrypt.compare(password, user.password)
-//     if (isMatch) return user
-//   } 
-//   return null
-// }
-
-// UserSchema.methods.toJSON = function () {
-//   const user = this
-//   const userObject = user.toObject()
-
-//   delete userObject.password
-//   delete userObject.__v
-
-//   return userObject
-// }
-
-// UserSchema.post("validate", function (error, doc, next) {
-//   if (error) {
-//     error.httpStatusCode = 400
-//     next(error)
-//   } else {
-//     next()
-//   }
-// })
-
-// UserSchema.pre("save", async function (next) {
-//   const user = this
-
-//   if (user.isModified("password")) {
-//     user.password = await bcrypt.hash(user.password, 8)
-//   }
-
-//   next()
-// })
-
-// UserSchema.post("save", function (error, doc, next) {
-//   if (error.name === "MongoError" && error.code === 11000) {
-//     error.httpStatusCode = 400
-//     next(error)
-//   } else {
-//     next()
-//   }
-// })
-
-// const UserModel = mongoose.model("User", UserSchema)
-
-// export default UserModel
